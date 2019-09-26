c83d25dd78d048b11efba067c0252544f743900e
logamurugan <sploga.murugan@gmail.com>
2019-09-26 16:01:32 +0530

    Move the hardcoded stuffs into config #14

--- server/configs/yelp.js
+++ server/configs/yelp.js
@@ -1,7 +1,7 @@
 
 const config = {
     "BASE_URL": 'https://api.yelp.com/v3/businesses/',
-    "API_KEY": 'bsS03gscuXbL4yiyXVE_nKDiv_wJ45wmsrN4bNYt8XJQtqrUBkyoWBesus6v7khf7KLtfOPxQ2X6W-12m8aEUa3dbCslmW9CV2A9uMT6AIyRVLJsOm1zwPzKn0mMXXYx'
+    "API_KEY": ''
 }
 
 module.exports = config;
